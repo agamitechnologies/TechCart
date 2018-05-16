5cb38ed2056e6a532594645e1b8a7392d37feba9
Deeksha Sharma <deeksha.sharma@agamitechnologies.com>
2018-05-16 12:48:48 +0530

    initial commit for vue integration with typescript

---
+++ build/webpack.base.config.js
@@ -0,0 +1,71 @@
+const path = require("path")
+
+const config = {
+    entry: {
+        app: path.resolve(__dirname, "../src/client-entry.ts")
+    },
+    module: {
+        rules: [
+
+            {
+                test: /\.ts$/,
+                exclude: /node_modules|vue\/src/,
+                loader: "ts-loader",
+                options: {
+                    appendTsSuffixTo: [/\.vue$/]
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.sass$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader?indentedSyntax'
+                ],
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js'
+        },
+        extensions: ['.ts', '.js', '.vue', '.json']
+    },
+    output: {
+        path: path.resolve(__dirname, "../dist"),
+        publicPath: "/",
+        filename: "assets/js/[name].js"
+    }
+}
+
+module.exports = config
---
+++ src/Test.vue
@@ -0,0 +1,15 @@
+<!-- src/components/App.vue -->
+<template> 
+<div>
+    <p>Hello Testing!</p>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style>
+</style>
